6827e3b6ce0b974010d0d73d2b5fd432b44871da
newmohib <newmohib@gmail.com>
2019-07-07 08:32:16 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+const development = {
+  database: 'users',
+  username: 'root',
+  password: '123456',
+  host: 'localhost',
+  dialect: 'mysql' || 'postgres',
+};
+
+const testing = {
+  database: 'databasename',
+  username: 'username',
+  password: 'password',
+  host: 'localhost',
+  dialect: 'sqlite' || 'mysql' || 'postgres',
+};
+
+const production = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'sqlite' || 'mysql' || 'postgres',
+};
+
+module.exports = {
+  development,
+  testing,
+  production,
+};
